f81258c113e5e3febf9839b864d94fb1f430adfd
Ikhode <132768132+MyKhode@users.noreply.github.com>
2024-12-07 10:52:41 +0700

    week 5, portfolio basic home and about me

---
+++ week5/portfolio/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+
+  <dev>
+
+    <Header />
+
+    <RouterView />
+  </dev>
+
+</template>
---
+++ week5/portfolio/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main class=" ml-32">
+    <TheWelcome />
+  </main>
+</template>
